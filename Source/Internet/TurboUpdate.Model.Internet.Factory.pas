8052ff9ccae45d6c3ddf69d84c9b9e17eea45ae2
RTrevisan20 <rtinformatica.ti@gmail.com>
2025-05-27 12:26:08 -0400

    Adionado factory de internet

--- Source/Internet/TurboUpdate.Model.Internet.Factory.pas
+++ Source/Internet/TurboUpdate.Model.Internet.Factory.pas
@@ -1,3 +1,21 @@
+{******************************************************************************}
+{                           ErrorSoft TurboUpdate                              }
+{                          ErrorSoft(c)  2016-2017                             }
+{                                                                              }
+{                     More beautiful things: errorsoft.org                     }
+{                                                                              }
+{           errorsoft@mail.ru | vk.com/errorsoft | github.com/errorcalc        }
+{              errorsoft@protonmail.ch | habrahabr.ru/user/error1024           }
+{                                                                              }
+{             Open this on github: github.com/errorcalc/TurboUpdate            }
+{                                                                              }
+{ You can order developing vcl/fmx components, please submit requests to mail. }
+{ Вы можете заказать разработку VCL/FMX компонента на заказ.                   }
+{******************************************************************************}
+{                                                                              }
+{Adicionado por Renato Trevisan Fork=https://github.com/Rtrevisan20/TurboUpdate}
+{added by Renato Trevisan Fork=https://github.com/Rtrevisan20/TurboUpdate      }
+{******************************************************************************}
 unit TurboUpdate.Model.Internet.Factory;
 
 interface
